edc4866cc4f40a437cf00b01be2d8222814b733f
r0stro <nyhsynamor@gmail.com>
2022-03-31 19:36:56 +0300

    hw1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
+import Users from "./components/Users/Users";
+import Posts from "./components/Posts/Posts";
+import Comments from "./components/Comments/Comments";
+
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div>
+            <div className={'wrap'}>
+                <Users/>
+                <Posts/>
+            </div>
+            <Comments/>
+        </div>
+    );
 }
 
 export default App;
