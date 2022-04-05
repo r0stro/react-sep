faeeeef1bed146f88b6e00907d8e53102154b749
r0stro <nyhsynamor@gmail.com>
2022-04-05 15:16:57 +0300

    hw2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import Users from "./components/users/Users";
+import UserDetails from "./components/userDetails/UserDetails";
+import Posts from "./components/posts/Posts";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+
+    const getUser = (user) => {
+        setUser(user);
+        setUserId(null)
+    }
+
+    const getUserId = (id) => {
+        setUserId(id);
+    }
+
+    let [user, setUser] = useState(null);
+    let [userId, setUserId] = useState(null);
+
+    return (
+        <div>
+            <div className={'wrap'}>
+                <Users getUser={getUser}/>
+                {user && <UserDetails user={user} getUserId={getUserId}/>}
+            </div>
+            {userId && <Posts userId={userId}/>}
+        </div>
+    );
 }
 
 export default App;
